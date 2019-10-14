e330011b46017da45b9c5ead610cebb730d612d1
umair <umaiahmed@etisalat.ae>
2019-10-14 13:48:04 +0400

    validation part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import "./App.css";
 import NavBar from "./components/navbar";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import MoviesForm from "./components/moviesForm";
+import LoginForm from './components/loginForm';
+import "./App.css";
+
 
 function App() {
   return (
@@ -14,12 +16,13 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm}></Route>
           <Route path="/movies/:id" component={MoviesForm}></Route>
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
           <Route path="/rentals" component={Rentals}></Route>
           <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies"></Redirect>
+          <Redirect from="/" exact to="/login"></Redirect>
           <Redirect to="/not-found"></Redirect>
         </Switch>
       </main>
---
+++ src/components/common/input.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Input = ({ name, label, value, error, onChange }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input
+        onChange={onChange}
+        value={value}
+        name={name}
+        id={name}
+        type="text"
+        className="form-control"
+      />
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+export default Input;
